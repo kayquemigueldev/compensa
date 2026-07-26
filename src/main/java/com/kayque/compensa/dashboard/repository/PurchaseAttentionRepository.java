b63cd565c0701b56f09a1b96736cc02f4ed3ed9b
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-07-26 06:34:37 -0300

    refactor: remove sistema legado de alertas do dashboard

--- src/main/java/com/kayque/compensa/dashboard/repository/PurchaseAttentionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kayque.compensa.dashboard.repository;
-
-import com.kayque.compensa.dashboard.model.PurchaseAttentionSummary;
-
-public interface PurchaseAttentionRepository {
-
-    PurchaseAttentionSummary summarize();
-}
